2f93794297b0537085cbe5aeabbe23d797023fa7
Oliver Freiberg <oliverfrei97@gmail.com>
2024-11-26 19:46:50 +0100

    add README

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# EBAC-Pratique | Pojeto 1
+# EBAC-Pratique | Pojeto 1 - Git
  Projeto 1 do curso de Git da EBAC Analise de Dados
+ 
+ Para executar o programa calculadora.py, digite comando no terminal:
+ ./script_calculadora.sh
+ 
+ 
--- script_calculadora.sh
+++ script_calculadora.sh
@@ -1,32 +1,3 @@
 #!/bin/zsh
 
-# Introdução de número por usuário
-num1 = input("Digite seu primeiro numero: ")
-num2 = input("Digite seu segundo numero: ")
-convert_num1 = float(num1)
-convert_num2 = float(num2)
-
-# Operações
-
-def operacao():
-  escolhaOperacao = input("Escolha a operação que deseja realizar: (1: soma; 2: subtração; 3: multiplicação; 4: divisão)")
-  if escolhaOperacao == '1':
-    print(convert_num1 + convert_num2)
-  elif escolhaOperacao == '2':
-      print(convert_num1 - convert_num2)
-  elif escolhaOperacao == '3':
-      print(convert_num1 * convert_num2)
-  elif escolhaOperacao == '4':
-      print(convert_num1 / convert_num2)
-  else:
-      print("Operação inválida. Tente novamente")
-
-# Execução
-operacao()
-continuarNumeros = input('Deseja continuar? (S/N)')
-
-while continuarNumeros == 'S':
-  operacao()
-  continuarNumeros = input('Deseja continuar? (S/N)')
-else:
-  print('Até a próxima!')
+python3 calculadora.py
