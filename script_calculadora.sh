58340d45b50d657b56ec61431833e663a156c402
Oliver Freiberg <oliverfrei97@gmail.com>
2024-11-26 19:43:25 +0100

    Consertar dever

--- script_calculadora.sh
+++ script_calculadora.sh
@@ -1,9 +1,32 @@
 #!/bin/zsh
 
-chmod +x calculadora.py
+# Introdução de número por usuário
+num1 = input("Digite seu primeiro numero: ")
+num2 = input("Digite seu segundo numero: ")
+convert_num1 = float(num1)
+convert_num2 = float(num2)
 
-chmod 755 calculadora.py
+# Operações
 
-./calculadora.py
+def operacao():
+  escolhaOperacao = input("Escolha a operação que deseja realizar: (1: soma; 2: subtração; 3: multiplicação; 4: divisão)")
+  if escolhaOperacao == '1':
+    print(convert_num1 + convert_num2)
+  elif escolhaOperacao == '2':
+      print(convert_num1 - convert_num2)
+  elif escolhaOperacao == '3':
+      print(convert_num1 * convert_num2)
+  elif escolhaOperacao == '4':
+      print(convert_num1 / convert_num2)
+  else:
+      print("Operação inválida. Tente novamente")
 
-python3 calculadora.py
+# Execução
+operacao()
+continuarNumeros = input('Deseja continuar? (S/N)')
+
+while continuarNumeros == 'S':
+  operacao()
+  continuarNumeros = input('Deseja continuar? (S/N)')
+else:
+  print('Até a próxima!')
